0a410d4b0bb24bb32bcacb7de1541483560060f3
santosh <santosh.ece06@gmail.com>
2016-05-08 19:09:56 +0530

    Initial commit

---
+++ app/src/main/java/demo/test/HelloAndorid.java
@@ -0,0 +1,12 @@
+package demo.test;
+
+/**
+ * Created by santosh on 5/8/16.
+ */
+public class HelloAndorid {
+    public static void main(String[] args) {
+        String str = "Hellooo";
+
+        System.out.println(str);
+    }
+}
