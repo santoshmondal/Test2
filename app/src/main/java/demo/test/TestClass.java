a75668ec22eaca5976c16418ac1c34a75889251a
santoshmondal <santoshmondal@users.noreply.github.com>
2016-05-08 19:18:18 +0530

    Update TestClass.java

--- app/src/main/java/demo/test/TestClass.java
+++ app/src/main/java/demo/test/TestClass.java
@@ -6,5 +6,7 @@ package demo.test;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("webtest");
+        
     }
 }
