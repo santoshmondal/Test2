2234d242b7bd0fe71e0e3733e507fa4f8e7a8cad
santosh <santosh.ece06@gmail.com>
2016-07-31 22:42:17 +0530

    Bound Service Demo

--- app/src/main/java/com/example/santosh/test1/MyService.java
+++ app/src/main/java/com/example/santosh/test1/MyService.java
@@ -2,15 +2,68 @@ package com.example.santosh.test1;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MyService extends Service {
+    private static final String TAG = MyService.class.getName();
+    private final IBinder mBinder = new LocalBinder();
+
+
     public MyService() {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.i(TAG, "On Create");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "On Start Command");
+
+        Toast.makeText(this, "At Service, and it started", Toast.LENGTH_SHORT).show();
+        return START_STICKY;
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.i(TAG, "On BIND");
+
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i(TAG, "On UN_BIND");
+
+        return super.onUnbind(intent);
+    }
+
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+
+        Log.i(TAG, "On RE_BIND");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(this, "At Service, and it stopped.", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "On DESTROY");
+    }
+
+
+    public class LocalBinder extends Binder {
+        MyService getService() {
+            return MyService.this;
+        }
     }
 }
--- app/src/main/java/com/example/santosh/test1/MyServiceActivity.java
+++ app/src/main/java/com/example/santosh/test1/MyServiceActivity.java
@@ -1,5 +1,9 @@
 package com.example.santosh.test1;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,9 +13,30 @@ import android.widget.Toast;
 
 public class MyServiceActivity extends AppCompatActivity implements View.OnClickListener {
     private String TAG = "MY_SERVICE_ACTIVITY";
+    private MyService boundService;
+    private boolean bouneded;
 
     private Button button30;
     private Button button31;
+
+    private Button button32;
+    private Button button33;
+
+    private Button button34;
+    private Button button35;
+
+    private ServiceConnection serviceConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            boundService = ((MyService.LocalBinder) service).getService();
+            bouneded = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            bouneded = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +47,32 @@ public class MyServiceActivity extends AppCompatActivity implements View.OnClick
 
         button30.setOnClickListener(this);
         button31.setOnClickListener(this);
+
+        button32.setOnClickListener(this);
+        button33.setOnClickListener(this);
+
+        button34.setOnClickListener(this);
+        button35.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if(bouneded){
+            boundService.unbindService(serviceConn);
+        }
     }
 
     private void init() {
         button30 = (Button) findViewById(R.id.button30);
         button31 = (Button) findViewById(R.id.button31);
+
+        button32 = (Button) findViewById(R.id.button32);
+        button33 = (Button) findViewById(R.id.button33);
+
+        button34 = (Button) findViewById(R.id.button34);
+        button35 = (Button) findViewById(R.id.button35);
     }
 
 
@@ -36,10 +82,37 @@ public class MyServiceActivity extends AppCompatActivity implements View.OnClick
 
         if(v.getId() == R.id.button30) {
 
-            Toast.makeText(this, "Button 30", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Mohan", Toast.LENGTH_SHORT).show();
         } else if(v.getId() == R.id.button31) {
 
-            Toast.makeText(this, "Button 31", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Jodaro", Toast.LENGTH_SHORT).show();
+        } else if(v.getId() == R.id.button32) {
+
+            Log.i(TAG, "Start Service");
+            Toast.makeText(this, "Start Service", Toast.LENGTH_SHORT).show();
+
+            Intent serviceIntent = new Intent(this, MyService.class);
+            startService(serviceIntent);
+        } else if(v.getId() == R.id.button33) {
+
+            Log.i(TAG, "Stop Service");
+            Toast.makeText(this, "Stop Service", Toast.LENGTH_SHORT).show();
+
+            Intent serviceIntent = new Intent(this, MyService.class);
+            stopService(serviceIntent);
+        } else if(v.getId() == R.id.button34) {
+
+            Log.i(TAG, "Bounded Service Start");
+            Toast.makeText(this, "Bound Service Start", Toast.LENGTH_SHORT).show();
+
+            Intent serviceIntent = new Intent(this, MyService.class);
+            bindService(serviceIntent, serviceConn, BIND_AUTO_CREATE);
+        } else if(v.getId() == R.id.button35) {
+
+            Log.i(TAG, "Unbound Service");
+            Toast.makeText(this, "Unbound Service", Toast.LENGTH_SHORT).show();
+
+            unbindService(serviceConn);
         }
     }
 }
